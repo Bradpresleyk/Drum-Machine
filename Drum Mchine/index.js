31c743f67295c0078eda38f6318d13baf64c73fb
Brad King <48492905+Bradpresleyk@users.noreply.github.com>
2020-04-20 17:10:57 +0200

    Add files via upload

---
+++ Drum Mchine/index.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#5de49a",
+    "#d42f2f",
+    "#c388cf",
+    "#ecea55",
+    "#9bb7d7",
+    "#199ed3"
+  ];
+
+  // Lets get going with the sounds
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+
+      createBubbles(index);
+    });
+  });
+
+  // Function that makes bubbles
+  const createBubbles = (index) => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    })
+  };
+});
